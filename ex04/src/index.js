23f5652ddb7be4954e671d6adebcb0dbf4818045
mladenpjevcevic98 <pjevcevicm237@gmail.com>
2021-09-22 17:32:34 +0200

    Miralem Pjanic

---
+++ ex04/src/index.js
@@ -0,0 +1,18 @@
+var players = {
+    7:'Muhamed Besic',
+    10: 'Miralem Pjanic', 
+    11: 'Edin Dzeko'
+};
+
+function myFunction(myObj)
+{
+    var playerNumber = players["10"];
+
+    var player = playerNumber;
+
+    return player;
+
+}
+
+console.log(myFunction(players));
+module.exports = {myFunction, players};
